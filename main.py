33c159122a2b2c0ec40164bb1d71142c68c82c38
Philipp Pfeil <rcpw.wiegand@me.com>
2023-12-05 09:17:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from days import one, two, three
+from utils import input
 
-one.run()
-two.run()
-three.run()
+data_input_one = input.getInputData("one")
+data_input_two = input.getInputData("two")
+data_input_three = input.getInputData("three")
+
+one.run(data_input_one)
+two.run(data_input_two)
+three.run(data_input_three)
