18895d0f38fcc104849004826702ce5ec40a85c4
Pfeil, P., ext., SNL IT P&P, BN, external, external <philipp.pfeil@deutschepost.de>
2023-12-21 22:41:00 +0100

    add cache

--- days/twenty.py
+++ days/twenty.py
@@ -1,2 +1,21 @@
 def run():
   print("")
+  
+  
+def flip_flop_module(pulse: str) -> str:
+  on = False
+  if pulse is "low":
+    on = not on
+    
+  if on is True: return 
+  if on is False: return
+  return 0
+
+def conjunction_module(pulse: str) -> str:
+  return 0
+
+def broadcast_module(pulse: str) -> str:
+  return 0
+
+def button_module(pulse: str) -> str:
+  return 0
